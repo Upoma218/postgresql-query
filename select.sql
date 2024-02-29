2e6bfebfbec8adc60480b2f7b1df495479b71409
Upoma21801~ <upoma.cdsr@gmail.com>
2024-03-01 03:38:56 +0600

    pagination by using OFFSET and LIMIT

--- select.sql
+++ select.sql
@@ -212,14 +212,14 @@ SELECT * from students
 
 
 
--- SELECT * FROM students 
---     WHERE country IN('USA', 'UK', 'Canada') LIMIT 5;
+SELECT * FROM students 
+    WHERE country IN('USA', 'UK', 'Canada') LIMIT 5;
 
 
--- SELECT * from students LIMIT 5 OFFSET 5 * 0;
--- SELECT * from students LIMIT 5 OFFSET 5 * 1;
--- SELECT * from students LIMIT 5 OFFSET 5 * 2;
--- SELECT * from students LIMIT 5 OFFSET 5 * 3;
+SELECT * from students LIMIT 5 OFFSET 5 * 0;
+SELECT * from students LIMIT 5 OFFSET 5 * 1;
+SELECT * from students LIMIT 5 OFFSET 5 * 2;
+SELECT * from students LIMIT 5 OFFSET 5 * 3;
 
 
 -- SELECT * FROM students WHERE country = 'USA' ;
